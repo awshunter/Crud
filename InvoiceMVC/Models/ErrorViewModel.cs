4f60487cf19198026415f09c47754836f3818368
Aws Zaid <zaidaws0@gmail.com>
2025-11-23 13:29:05 +0300

    Revert "Add project files."
    This reverts commit 084b5dc39304a543e791a7e4d2fa6ddb8f3ba49a.

--- InvoiceMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InvoiceMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
